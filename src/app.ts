f2b8d0888ddc14f48bc97e6862f7295eb4d74941
Ojo-williams Daniel <freakydmuse@gmail.com>
2024-06-14 17:42:57 +0100

    create: project init

---
+++ src/app.ts
@@ -0,0 +1,5 @@
+function passwordGenerator(strength: string, length: number) {
+  const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+  const digits = 1234567890
+}
